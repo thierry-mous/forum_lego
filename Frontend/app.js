2b3c5013bef1b71c823dcc33be30e7ebb6b488c8
prplecloud <52612580+prplecloud@users.noreply.github.com>
2024-06-25 10:50:47 +0200

    new routes

--- Frontend/app.js
+++ Frontend/app.js
@@ -11,25 +11,32 @@ app.use('/public/', express.static(join(__dirname, 'public')));
 const { fileURLToPath } = require('url');
 
 app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'templates', 'index.html'));
+    res.sendFile(join(dirname, 'templates', 'index.html'));
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(join(__dirname, 'templates', 'connexion.html'));
+    res.sendFile(join(dirname, 'templates', 'connexion.html'));
 });
 
 app.get('/sign_up', (req, res) => {
-    res.sendFile(join(__dirname, 'templates', 'inscription.html'));
+    res.sendFile(join(dirname, 'templates', 'inscription.html'));
 });
 
 app.get('/topics', (req, res) => {
-    res.sendFile(join(__dirname, 'templates', 'topics.html'));
+    res.sendFile(join(dirname, 'templates', 'topics.html'));
 });
 
 app.get('/profile', (req, res) => {
     res.sendFile(join(__dirname, 'templates', 'profile.html'));
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(join(__dirname, 'templates', 'about.html'));
+});
+
+app.get('/legalmentions', (req, res) => {
+    res.sendFile(join(__dirname, 'templates', 'legalmentions.html'));
+});
 
 
 const PORT = process.env.PORT || 8080;
