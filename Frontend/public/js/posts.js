fa785083b6acc8675764a81059721db4eeb3e49b
thierry-mous <thierry.mousnier@ynov.com>
2024-06-27 12:29:49 +0200

    post

--- Frontend/public/js/posts.js
+++ Frontend/public/js/posts.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
     const topicId = urlParams.get('id');
 
@@ -8,29 +8,62 @@ document.addEventListener('DOMContentLoaded', function() {
             'Content-Type': 'application/json',
         },
     })
-    .then(response => response.json())
-    .then(posts => {
-        const postsContainer = document.getElementById('postsContainer');
-        postsContainer.innerHTML = '';
-        posts.forEach(post => {
-            const postElement = document.createElement('div');
-            postElement.classList.add('post');
-            postElement.innerHTML = `
-                <div class="user-info">
-                    <p class="username">${post.author}</p>
-                    <p class="user-role">${post.admin_status ? post.admin_status : 'User'}</p>
-                </div>
-                <div class="post-content">
-                    <p class="post-date">Posted on: ${new Date(post.publish_date).toLocaleDateString()}</p>
-                    <p class="post-text">${post.body}</p>
-                </div>
-            `;
-            postsContainer.appendChild(postElement);
-        });
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-});
+        .then(response => response.json())
+        .then(posts => {
+            const postsContainer = document.getElementById('postsContainer');
+            postsContainer.innerHTML = '';
 
-       
+            if (posts.length > 0) {
+                const firstPost = posts[0];
+                const firstPostElement = document.createElement('div');
+                firstPostElement.classList.add('first-post');
+                firstPostElement.innerHTML = `
+            <div class="center-container">
+                <div class="post1">
+                    <div class="user-info">
+                    <div class="infopost">
+                        <p class="username">${firstPost.author}</p>
+                         <p class="user-role">${firstPost.admin_status ? firstPost.admin_status : 'User'}</p>
+                    </div>
+                        <p class="post-date">Posted on: ${new Date(firstPost.publish_date).toLocaleDateString()}</p>
+                    </div>
+                    <div class="post-content">
+                    <p class="first-text">${firstPost.body}</p>
+                    </div>
+                </div>
+            </div>
+            `;
+                postsContainer.appendChild(firstPostElement);
+
+                const otherPostsContainer = document.createElement('div');
+                otherPostsContainer.classList.add('other-posts');
+
+                posts.slice(1).forEach(post => {
+                    const postElement = document.createElement('div');
+                    postElement.classList.add('post');
+                    postElement.innerHTML = `
+                    <div class="center-container">
+                    <div class="comment">
+                        <div class="user-info">
+                        <div class="infopost">
+                            <p class="username">${post.author}</p>
+                            <p class="user-role">${post.admin_status ? post.admin_status : 'User'}</p>
+                        </div>
+                            <p class="post-date">Posted on: ${new Date(post.publish_date).toLocaleDateString()}</p>
+                        </div>
+                        <div class="post-content">
+                            <p class="post-text">${post.body}</p>
+                        </div>
+                    </div>
+                    </div>
+                `;
+                    otherPostsContainer.appendChild(postElement);
+                });
+
+                postsContainer.appendChild(otherPostsContainer);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
